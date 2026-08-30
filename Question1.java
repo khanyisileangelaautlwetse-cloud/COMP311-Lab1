98e8ac9b0fdb195e5743908ef317b5533962a0aa
Khanyisile Autlwetse <khanyisileangelaautlwetse@gmail.com>
2026-08-30 23:18:25 +0200

    Add Question 1: personal profile

---
+++ Question1.java
@@ -0,0 +1,15 @@
+public class Question1 {
+
+    public static void main(String[] args) {
+
+        String name = "Khanyisile Autlwetse";
+        int age = 20;
+        double gpa = 3.6;
+        boolean fullTime = true;
+
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("GPA: " + gpa);
+        System.out.println("Full-time: " + fullTime);
+    }
+}
