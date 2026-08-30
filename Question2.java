8003f252d1ca89d8779251c0323716a369de578c
Khanyisile Autlwetse <khanyisileangelaautlwetse@gmail.com>
2026-08-30 23:27:06 +0200

    Add Question 2: primitive playground

---
+++ Question2.java
@@ -0,0 +1,23 @@
+public class Question2 {
+
+    public static void main(String[] args) {
+
+        byte myByte = 100;
+        short myShort = 20000;
+        int myInt = 350000;
+        long myLong = 9000000000L;
+        float myFloat = 3.14f;
+        double myDouble = 3.14159265;
+        char myChar = 'J';
+        boolean myBoolean = true;
+
+        System.out.println("byte: " + myByte);
+        System.out.println("short: " + myShort);
+        System.out.println("int: " + myInt);
+        System.out.println("long: " + myLong);
+        System.out.println("float: " + myFloat);
+        System.out.println("double: " + myDouble);
+        System.out.println("char: " + myChar);
+        System.out.println("boolean: " + myBoolean);
+    }
+}
